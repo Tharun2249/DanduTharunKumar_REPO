3d3d28fdf8e07d745a630c361ede409e8d99a4a2
Tharun2249 <76117512+Tharun2249@users.noreply.github.com>
2024-08-30 15:40:33 +0530

    Update StudentGetSetMain.java

--- Java Day 2 Assignment/src/day2/StudentGetSetMain.java
+++ Java Day 2 Assignment/src/day2/StudentGetSetMain.java
@@ -4,7 +4,7 @@ public class StudentGetSetMain {
 	public static void main(String[] args) {
 		StudentGetSet obj=new StudentGetSet();
 		obj.setStdid(101);
-		obj.setStdname("veda");
+		obj.setStdname("Tharun");
 		System.out.println(obj);
 		}
 }
